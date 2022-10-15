ff3413caced189e318e24e514905a14602624435
Arvind Singh <arvindsingh@Arvinds-MacBook-Pro.local>
2022-10-15 11:49:51 +0530

    first pull

---
+++ SampleMySDK/SampleMySDK/SampleMySDK.h
@@ -0,0 +1,18 @@
+//
+//  SampleSDk.h
+//  SampleSDk
+//
+//  Created by Arvind Singh on 15/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleSDk.
+FOUNDATION_EXPORT double SampleMySDKVersionNumber;
+
+//! Project version string for SampleSDk.
+FOUNDATION_EXPORT const unsigned char SampleMySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSDk/PublicHeader.h>
+
+
